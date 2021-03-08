0bb7f0bf3fba23ec2790c748f0ab72ef3fa5c30e
MilosTanaskovic <milos.tanaskovic@svenskadomaner.se>
2021-03-08 12:43:01 +0100

    accordion landing page

---
+++ 4-accordion/src/App.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import Question from './components/Question';
+import data from './data';
+import './App.css';
+
+function App() {
+  const [questions, setQuestions] = useState(data);
+  return (
+    <main>
+      <div className='container'>
+        <h3>questions and answers about login</h3>
+        <section className='info'>
+          {
+            questions.map((question) => {
+              return (
+                <Question 
+                key={question.id}
+                {...question} 
+              />
+              )
+            })
+          }
+        </section>
+      </div>
+    </main>
+  );
+}
+
+export default App;
